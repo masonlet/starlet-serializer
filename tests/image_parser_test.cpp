6dccba1528cf48f08892f52156a1641e04a2aebf
Masonlet <masonletoile@hotmail.com>
2025-11-18 11:46:57 -0500

    Split mesh parser tests and PLY parser tests into seperate files

--- tests/image_parser_test.cpp
+++ tests/image_parser_test.cpp
@@ -1,5 +1,8 @@
 #include <gtest/gtest.h>
 
+#include "starlet-serializer/parser/imageParser.hpp"
+#include "starlet-serializer/data/imageData.hpp"
+
 TEST(SerializerTest, ImagePlaceHolder) {
   EXPECT_TRUE(true);
 }
