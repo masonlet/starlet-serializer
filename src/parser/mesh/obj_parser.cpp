55f8ff3897b18e598dbc8a1929dd530bdbc962dd
Masonlet <masonletoile@hotmail.com>
2025-11-25 23:49:09 -0500

    Vertex position parsing

--- src/parser/mesh/obj_parser.cpp
+++ src/parser/mesh/obj_parser.cpp
@@ -1,10 +1,12 @@
 #include "starlet-serializer/parser/mesh/obj_parser.hpp"
 #include "starlet-serializer/data/mesh_data.hpp"
-
 #include "starlet-logger/logger.hpp"
+
+#include "starlet-math/vec3.hpp"
 
 #include <cstring>  
 #include <cfloat>
+#include <vector>
 
 namespace Starlet::Serializer {
 
@@ -23,8 +25,9 @@ bool ObjParser::parse(const std::string& path, MeshData& out) {
 	if (file.empty() || file[0] == '\0')
 		return Logger::error("ObjParser", "parse", "File is empty");
 
-  const unsigned char* p = file.data();
+	std::vector<Starlet::Math::Vec3<float>> positions;
 
+  const unsigned char* p = file.data();
   while (*p) {
     p = skipWhitespace(p);
     if (*p == '\0') break;
@@ -40,10 +43,26 @@ bool ObjParser::parse(const std::string& path, MeshData& out) {
       continue;
     }
 
-		// TODO: Impelement vertex/face parsing
+		if (strcmp((const char*)cmd, "v") == 0) {
+			Starlet::Math::Vec3<float> pos;
+			if (!parseFloat(p, pos.x) || !parseFloat(p, pos.y) || !parseFloat(p, pos.z))
+				return Logger::error("ObjParser", "parse", "Failed to parse vertex position at vertex " + std::to_string(positions.size()));
 
-    p = skipToNextLine(p);
+			float w;
+			parseFloat(p, w); 
+
+			positions.push_back(pos);
+		}
+		else {
+			p = skipToNextLine(p);
+		}
   }
+
+	out.numVertices = positions.size();
+	out.vertices.resize(out.numVertices);
+	for (size_t i = 0; i < out.numVertices; ++i) {
+		out.vertices[i].pos = positions[i];
+	}
 
 	return true;
 }
--- tests/mesh/obj_parser_test.cpp
+++ tests/mesh/obj_parser_test.cpp
@@ -8,6 +8,35 @@ TEST_F(ObjParserTest, CommentOnly) {
   EXPECT_TRUE(parser.parse("test_data/comments.obj", out));
 }
 
+TEST_F(ObjParserTest, SingleVertex) {
+  createTestFile("test_data/single_vertex.obj", "v 1.0 2.0 3.0\n");
+  EXPECT_TRUE(parser.parse("test_data/single_vertex.obj", out));
+  EXPECT_EQ(out.numVertices, 1);
+  EXPECT_FLOAT_EQ(out.vertices[0].pos.x, 1.0f);
+  EXPECT_FLOAT_EQ(out.vertices[0].pos.y, 2.0f);
+  EXPECT_FLOAT_EQ(out.vertices[0].pos.z, 3.0f);
+}
+
+TEST_F(ObjParserTest, MultipleVertices) {
+  createTestFile("test_data/vertices.obj", "v 0.0 0.0 0.0\nv 1.0 0.0 0.0\nv 0.0 1.0 0.0\n");
+  EXPECT_TRUE(parser.parse("test_data/vertices.obj", out));
+  EXPECT_EQ(out.numVertices, 3);
+  EXPECT_FLOAT_EQ(out.vertices[2].pos.y, 1.0f);
+}
+
+TEST_F(ObjParserTest, VertexWithW) {
+  createTestFile("test_data/vertex_w.obj", "v 1.0 2.0 3.0 0.5\n");
+  EXPECT_TRUE(parser.parse("test_data/vertex_w.obj", out));
+  EXPECT_EQ(out.numVertices, 1);
+  EXPECT_FLOAT_EQ(out.vertices[0].pos.z, 3.0f);
+}
+
+TEST_F(ObjParserTest, VerticesWithComments) {
+  createTestFile("test_data/vert_comments.obj", "# Header\nv 1.0 0.0 0.0\n# Mid comment\nv 0.0 1.0 0.0\n");
+  EXPECT_TRUE(parser.parse("test_data/vert_comments.obj", out));
+  EXPECT_EQ(out.numVertices, 2);
+}
+
 
 // Error tests
 TEST_F(ObjParserTest, EmptyFile) {
@@ -17,4 +46,20 @@ TEST_F(ObjParserTest, EmptyFile) {
   EXPECT_FALSE(parser.parse("test_data/empty.obj", out));
   std::string output = testing::internal::GetCapturedStderr();
   EXPECT_NE(output.find("File is empty"), std::string::npos);
+}
+
+TEST_F(ObjParserTest, VertexMissingZ) {
+  createTestFile("test_data/missing_z.obj", "v 1.0 2.0\n");
+  testing::internal::CaptureStderr();
+  EXPECT_FALSE(parser.parse("test_data/missing_z.obj", out));
+  std::string output = testing::internal::GetCapturedStderr();
+  EXPECT_NE(output.find("Failed to parse vertex position at vertex 0"), std::string::npos);
+}
+
+TEST_F(ObjParserTest, VertexInvalidFloat) {
+  createTestFile("test_data/invalid_float.obj", "v 1.0 abc 3.0\n");
+  testing::internal::CaptureStderr();
+  EXPECT_FALSE(parser.parse("test_data/invalid_float.obj", out));
+  std::string output = testing::internal::GetCapturedStderr();
+  EXPECT_NE(output.find("Failed to parse vertex position"), std::string::npos);
 }
